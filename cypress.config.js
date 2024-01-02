ff8149a97510273bf6d2ab1d455daebd6e2b88e8
Hian <contato.hian@outlook.com>
2024-01-02 11:51:52 -0300

    .

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    experimentalRunAllSpecs: true,
+    experimentalStudio: true,
+  },
+};
